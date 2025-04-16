5e36718de6f660815466c6bbc0f5421ddd26c128
ARI-marker <arisheqiri833@gmail.com>
2025-04-16 19:17:31 +0200

    punoi ari baba 2.0
    uiuiui

---
+++ payment.js
@@ -0,0 +1,9 @@
+document.getElementById("paymentForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    
+    document.getElementById("confirmation").classList.remove("hidden");
+  
+    
+  });
+  
